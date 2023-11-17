398c16f953e58bc8f615c01ef7bc3e061170008c
Tarık Saraç <78061895+t4r7k@users.noreply.github.com>
2023-11-17 23:33:24 +0300

    websocket for qr forwarding

--- service/index.js
+++ service/index.js
@@ -1,65 +1,33 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const multer = require('multer')
-const cors = require('cors')
-const compression = require('compression')
-const helmet = require('helmet')
-const dotenv = require('dotenv')
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
 
-const test = require('./src/routes/test.route')
+const app = express();
+const port = process.env.PORT || 4000;
 
-dotenv.config()
+app.use(cors());
 
-const app = express()
-const port = process.env.PORT || 3000
+const server = http.createServer(app);
+const io = socketIo(server);
 
-// Multer configuration
-const upload = multer({
-    storage: multer.memoryStorage(),
-    limits: { fileSize: 5 * 1024 * 1024 }, // no larger than 5mb
-})
+const sessions = {}; // To keep track of sessions and UIDs
 
-// Error handler
-const errorHandler = (error, request, response, next) => {
-    if (!error) {
-        response.status(404).send('Page not found')
-        return
-    }
+io.on("connection", (socket) => {
+    socket.on("register", (uid) => {
+        console.log("Registered UID: " + uid + " Socket Id: " + socket.id);
+        sessions[uid] = socket.id;
+    });
 
-    console.error(error.message)
+    socket.on("message", ({ uid, message }) => {
+        console.log("Got Message! UID: " + uid + " Message: " + message)
+        const targetSocketId = sessions[uid];
+        console.log("Target Socket Id: " + targetSocketId)
+        if (uid) {
+            console.log("Sending message to: " + uid);
+            io.to(uid).emit("message", message);
+        }
+    });
+});
 
-    const status = error.status || 400
-    response.status(status).send("Internal Server Error")
-}
-
-// Middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(
-    helmet({
-        crossOriginResourcePolicy: {
-            policy: 'cross-origin',
-        },
-    })
-)
-app.use(cors())
-app.use(compression())
-app.use(
-    helmet({
-        crossOriginResourcePolicy: {
-            policy: 'cross-origin',
-        },
-    })
-)
-
-// Routes
-app.get('/', (req, res) => res.json({ message: 'ok' }))
-app.use('/qr', test)
-
-// Error handler middleware
-app.use(errorHandler)
-
-// Start the server
-app.listen(port, () => console.log(`App listening on port ${port}!`))
-
-module.exports = app
+server.listen(port, () => console.log("Server running on port 4000"));
