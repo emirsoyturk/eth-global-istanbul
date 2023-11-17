039ae74226e37f4b9d5f11a145804465bc28cc76
Emir Soyturk <emirsytrk@gmail.com>
2023-11-17 23:36:18 +0300

    proof of inside and proof of outside using polygon algorithm

--- noir/proof_of_location/sign.js
+++ noir/proof_of_location/sign.js
@@ -1,24 +1,21 @@
 const { ethers } = require("ethers");
 const { fromHex, hashMessage, recoverPublicKey } = require("viem");
 
-let messageToHash =
-  "0xabfd76608112cc843dca3a31931f3974da5f9f5d32833e7817bc7e5c50c7821e";
+let message = "0x0".padEnd(66, "0");
 
 async function getInfos() {
   const sender = new ethers.Wallet(
     "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
   );
 
-  const hashedMessage = hashMessage(messageToHash, "hex"); // keccak of "signthis"
+  const signature = await sender.signMessage(message);
 
-  const signature = await sender.signMessage(hashedMessage); // get the signature of the message, this will be 130 bytes (concatenated r, s, and v)
-
-  const pubKey = await recoverPublicKey({ hash: hashedMessage, signature });
+  const pubKey = await recoverPublicKey({ hash: message, signature });
 
   return {
     pub_key: [...fromHex(pubKey, "bytes").slice(1)],
     signature: [...fromHex(signature, "bytes").slice(0, 64)],
-    hashed_message: [...fromHex(hashedMessage, "bytes")],
+    message: [...fromHex(message, "bytes")],
   };
 }
 
@@ -27,7 +24,7 @@ async function main() {
   console.log(res["pub_key"].slice(0, 32));
   console.log(res["pub_key"].slice(32));
   console.log(res["signature"]);
-  console.log(res["hashed_message"]);
+  console.log(res["message"]);
 }
 
 main();
--- service/index.js
+++ service/index.js
@@ -14,20 +14,22 @@ const io = socketIo(server);
 const sessions = {}; // To keep track of sessions and UIDs
 
 io.on("connection", (socket) => {
-    socket.on("register", (uid) => {
-        console.log("Registered UID: " + uid + " Socket Id: " + socket.id);
-        sessions[uid] = socket.id;
-    });
+  socket.on("register", (uid) => {
+    console.log("Registered UID: " + uid + " Socket Id: " + socket.id);
+    sessions[uid] = socket.id;
+  });
 
-    socket.on("message", ({ uid, message }) => {
-        console.log("Got Message! UID: " + uid + " Message: " + message)
-        const targetSocketId = sessions[uid];
-        console.log("Target Socket Id: " + targetSocketId)
-        if (uid) {
-            console.log("Sending message to: " + uid);
-            io.to(uid).emit("message", message);
-        }
-    });
+  socket.on("message", ({ uid, message }) => {
+    console.log("Got Message! UID: " + uid + " Message: " + message);
+    const targetSocketId = sessions[uid];
+    console.log("Target Socket Id: " + targetSocketId);
+    if (uid) {
+      console.log("Sending message to: " + uid);
+      io.to(uid).emit("message", message);
+    }
+  });
 });
+
+app.use("/noir", noir);
 
 server.listen(port, () => console.log("Server running on port 4000"));
