c82a5131bf1f25e06543c700a4a0831e9f18618b
Emir Soyturk <emirsytrk@gmail.com>
2023-11-18 11:43:35 +0300

    new seeder.js

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -7,21 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Map = await hre.ethers.getContractFactory("Map");
+  const map = await Map.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  await map.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(`Map deployed to ${map.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
