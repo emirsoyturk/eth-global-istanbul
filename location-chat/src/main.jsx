9e0b6bf27c53dd66d159605077be70b6907c3f38
Emir Soyturk <emirsytrk@gmail.com>
2023-11-18 13:15:51 +0300

    initial push app

---
+++ location-chat/src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./output.css";
+
+import { ChatUIProvider } from "@pushprotocol/uiweb";
+import { darkChatTheme } from "@pushprotocol/uiweb";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ChatUIProvider theme={darkChatTheme}>
+    <App />
+  </ChatUIProvider>
+);
