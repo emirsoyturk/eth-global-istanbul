c2338b92f91e0107ffe61345a7933082da23a304
Emir Soyturk <emirsytrk@gmail.com>
2023-11-18 07:25:55 +0300

    footer color

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,42 +1,52 @@
 import React from "react";
-import { MdSettings } from 'react-icons/md';
+import { MdSettings } from "react-icons/md";
 import { FaHome } from "react-icons/fa";
 import { SiGoogleanalytics } from "react-icons/si";
 import { MdOutlineQrCodeScanner } from "react-icons/md";
-import FooterBG from '../../Images/footer_bg.svg'; // Import your image
 
 import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="w-full sticky bottom-0 flex justify-center items-end p-4">
-      <img className='absolute w-full z-[-1] bottom-0' src={FooterBG} alt="Background" />
+      <img
+        className="absolute w-full z-[-1] bottom-0"
+        src={"/rectangle.svg"}
+        alt="Background"
+      />
       <div className="flex flex-row justify-around items-center text-softPurple w-full max-w-[100%] mb-[4vw]">
         <div>
-        <Link to={'./landingPage'}> <FaHome size={'11vw'} className="cursor-pointer text-pink" /> </Link >
+          <Link to={"./landingPage"}>
+            {" "}
+            <FaHome size={"40"} className="cursor-pointer text-pink" />{" "}
+          </Link>
         </div>
         <div>
-          <FaHome size={'11vw'} className="cursor-pointer text-pink" />
+          <FaHome size={"40"} className="cursor-pointer text-pink" />
         </div>
       </div>
 
       <div className="cursor-pointer">
-        <Link to={'./qr'}>
-          <div className="flex justify-center items-center bg-purple rounded-full p-1 mb-[14vw]">
-            <MdOutlineQrCodeScanner size={'17vw'} className="text-purpleBlack p-2"/>
+        <Link to={"./qr"}>
+          <div className="flex justify-center items-center bg-[#55389B] rounded-full p-1 mb-[14vw]">
+            <MdOutlineQrCodeScanner
+              size={"80"}
+              strokeWidth={0.01}
+              className="text-white p-3"
+            />
           </div>
         </Link>
       </div>
 
       <div className="flex flex-row justify-around items-center text-softPurple w-full max-w-[100%] mb-[4vw]">
         <div>
-          <SiGoogleanalytics size={'11vw'} className="cursor-pointer text-pink" />
+          <SiGoogleanalytics size={"40"} className="cursor-pointer text-pink" />
         </div>
         <div>
-          <MdSettings size={'11vw'} className="cursor-pointer text-pink" />
+          <MdSettings size={"40"} className="cursor-pointer text-pink" />
         </div>
       </div>
-      <div className="absolute bottom-0 w-full h-[12vw] z-[-1] bg-purple"></div>
+      <div className="absolute -bottom-2 w-full h-[13vw] z-[-1] bg-[#2C2868]"></div>
     </div>
   );
 };
--- client/src/Pages/Mainpage/Mainpage.js
+++ client/src/Pages/Mainpage/Mainpage.js
@@ -1,20 +1,22 @@
-import CardItem from '../../Components/CardItem/CardItem';
-import SmallCards from '../../Components/SmallCardItem/SmallCards';
-import DefaultImage from '../../Images/img1.png'
-import Header from '../../Components/Header/Header';
-
+import CardItem from "../../Components/CardItem/CardItem";
+import SmallCards from "../../Components/SmallCardItem/SmallCards";
+import DefaultImage from "../../Images/img1.png";
+import Header from "../../Components/Header/Header";
 
 function MainPage() {
-    return (
-            <div className="flex flex-col h-full text-purple font-bold flex-grow mx-auto max-w-screen-xl">
-                <Header />
-                <div className="flex-grow mx-auto max-w-screen-xl">
-                    <CardItem title={"2.70% Today"} subtitle={"$12,670.90"} imageSrc={DefaultImage} />
-                    <SmallCards />
-                </div>
-            </div>
-    );
+  return (
+    <div className="flex flex-col h-full text-purple font-bold flex-grow mx-auto max-w-screen-xl">
+      <Header />
+      <div className="flex-grow mx-auto max-w-screen-xl">
+        <CardItem
+          title={"2.70% Today"}
+          subtitle={"$12,670.90"}
+          imageSrc={DefaultImage}
+        />
+        <SmallCards />
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
-
